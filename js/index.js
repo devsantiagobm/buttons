3d6624b864bc3a2917616b0ca2c752093b8c517f
santiago <sanseb290514@gmail.com>
2022-01-09 22:47:45 -0500

    Button Gallery
    A button gallery that could help you add original buttons to your website. With basic functionalities and design

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header__texto, .header__imagenes', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px',
+    });
+
+    sr.reveal('.button, .section__descarga',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px',
+    })
