eba76d89f2400b80fb41da94c85ecdaf3c0ef388
santiago <sanseb290514@gmail.com>
2022-01-16 18:15:05 -0500

    test commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.header__texto, .header__imagenes', {
-        duration: 2000,
-        origin: 'bottom',
-        distance: '100px',
-    });
-
     sr.reveal('.button, .section__descarga',{
         duration: 2000,
         origin: 'bottom',
